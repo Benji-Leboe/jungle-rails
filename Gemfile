b00e4f6019cd872a568d1aea913af070f1c54f81
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-10-16 13:14:53 -0700

    setup feature specs

--- Gemfile
+++ Gemfile
@@ -41,11 +41,17 @@ gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'phantomjs', :require => 'phantomjs/poltergeist'
   gem 'byebug'
   gem 'quiet_assets'
   gem 'dotenv-rails'
   gem 'rspec-rails', '~> 3.5'
   gem 'database_cleaner'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'poltergeist'
 end
 
 group :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,29 +14,29 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  require 'database_cleaner'
-  # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
+  # require 'database_cleaner'
+  # # rspec-expectations config goes here. You can use an alternate
+  # # assertion/expectation library such as wrong or the stdlib/minitest
+  # # assertions if you prefer.
+  # config.before(:suite) do
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
  
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
+  # config.before(:each) do
+  #   DatabaseCleaner.strategy = :transaction
+  # end
  
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
-  end
+  # config.before(:each, :js => true) do
+  #   DatabaseCleaner.strategy = :truncation
+  # end
  
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
+  # config.before(:each) do
+  #   DatabaseCleaner.start
+  # end
  
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
+  # config.after(:each) do
+  #   DatabaseCleaner.clean
+  # end
 
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
